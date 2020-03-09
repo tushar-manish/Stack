095b2401a29a58f92ec12794ee791946a10b91a8
tushar-manish <tushargupta9412@gmail.com>
2020-03-09 07:06:03 -1000

    Stack Main Class

---
+++ src/Stack_Main.java
@@ -0,0 +1,9 @@
+public class Stack_Main {
+    public static void main(String[] args) {
+        Stack_implement st = new Stack_implement();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        System.out.println(st.peek());
+    }
+}
